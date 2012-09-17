2699943389092b5c5c1619744f75d0cadaff48f4
Tomasz Pewiński <pewniak747@gmail.com>
2012-09-17 14:52:56 +0200

    updated middleman

--- config.rb
+++ config.rb
@@ -1,28 +1,12 @@
 activate :blog
 activate :directory_indexes
+activate :syntax
 set :blog_permalink, ":year/:month/:day/:title"
-# set :blog_summary_separator, /READMORE/
-# set :blog_summary_length, 500
-#
 set :blog_layout_engine, :haml
 
 page "/feed.xml", :layout => false
 
-# Build-specific configuration
-configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-  
-  # Minify Javascript on build
-  # activate :minify_javascript
-  
-  # Enable cache buster
-  # activate :cache_buster
-end
-
-require 'rack/codehighlighter'
-use Rack::Codehighlighter, 
-  :pygments_api,
-  :element => "pre>code",
-  :pattern => /\A:::([-_+\w]+)\s*\n/,
-  :markdown => true
+set :markdown_engine, :redcarpet
+set :markdown, :fenced_code_blocks => true,
+               :autolink => true,
+               :smartypants => true
