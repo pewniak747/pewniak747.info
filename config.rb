4e193bd04f8ac07050467bdbafb0b7000906f358
Tomasz Pewiński <pewniak747@gmail.com>
2016-12-05 17:24:38 +0100

    upgrade middleman to v4

--- config.rb
+++ config.rb
@@ -1,3 +1,16 @@
+class RewriteMarkdownPaths < Middleman::Extension
+  # https://github.com/middleman/middleman-blog/issues/277
+  def manipulate_resource_list(resources)
+    resources.each do |resource|
+      next unless resource.source_file.end_with?('.markdown')
+      resource.options[:content_type] = "text/html"
+      resource.destination_path << "/index.html"
+    end
+  end
+end
+
+::Middleman::Extensions.register(:rewrite_markdown_paths, RewriteMarkdownPaths)
+
 activate :blog do |blog|
   blog.sources = 'articles/:year-:month-:day-:title'
   blog.permalink = ':year/:month/:day/:title'
@@ -5,13 +18,12 @@ end
 
 activate :syntax
 activate :directory_indexes
-activate :bourbon
-set :blog_layout_engine, :haml
-set :haml, { ugly: true }
+activate :rewrite_markdown_paths
+set :haml, { ugly: true, format: :html5 }
 
-page "/feed.xml", :layout => false
+page "/feed.xml", layout: false
 
 set :markdown_engine, :redcarpet
-set :markdown, :fenced_code_blocks => true,
-               :autolink => true,
-               :smartypants => true
+set :markdown, fenced_code_blocks: true,
+               autolink: true,
+               smartypants: true
